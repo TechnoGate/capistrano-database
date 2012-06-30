abf45009c6523a202ab109aa5a6357f7a8ea419c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2012-06-30 23:46:31 +0200

    Implement generating and writing the credentials

--- lib/capistrano/ext/database.rb
+++ lib/capistrano/ext/database.rb
@@ -38,11 +38,36 @@ Capistrano::Configuration.instance.load do
 
       desc "[internal] Generate the database server #{method}"
       task "generate_#{method}", :roles => :app do
+        set "db_#{method}".to_sym, {
+          hostname: ask('What is the hostname used to access the database',
+                        default: 'localhost',
+                        validate: /.+/),
+          port:     ask('What is the port used to access the database',
+                        default: '',
+                        validate: /\A[0-9]*\Z/),
+          username: ask('What is the username used to access the database',
+                        default: fetch(:db_username),
+                        validate: /.+/),
+          password: ask('What is the password used to access the database',
+                        default: '',
+                        validate: /.+/,
+                        echo: false),
+        }
+      end
 
+      desc "[internal] Write the database server #{method}"
+      task "write_#{method}", :roles => :app do
+        write fetch("db_#{method}_file".to_sym),
+          format_credentials(fetch "db_#{method}".to_sym)
       end
     end
   end
 
+  # Dependencies
   before 'db:print_credentials', 'db:credentials'
   before 'db:print_root_credentials', 'db:root_credentials'
+
+  ['credentials', 'root_credentials'].each do |method|
+    after "db:generate_#{method}", "db:write_#{method}"
+  end
 end
