55735e4c14e3ebb1e515aeae7d44cba2ea11033c
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2012-06-30 19:57:31 +0200

    Initial Import

---
+++ lib/capistrano-database.rb
@@ -0,0 +1,7 @@
+require "capistrano-database/version"
+
+module Capistrano
+  module Database
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano-database/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Database
+    VERSION = "0.0.1"
+  end
+end
